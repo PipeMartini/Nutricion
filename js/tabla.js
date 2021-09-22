495f268cd068b006daadfa425d5d0abab8fcb4b2
Felipe <86312185+PipeMartini@users.noreply.github.com>
2021-09-21 23:18:53 -0300

    Add files via upload

--- js/tabla.js
+++ js/tabla.js
@@ -1,14 +1,80 @@
-
-
-
-let acumuladorDietas=``;
+//              dietasrecibidas[0].area    para llamar al array traido del local storage. [0] llama al primer valor
 
 let traerDatos = function () {
-    let dietasrecibidas = JSON.stringify(localStorage.getItem('dietas'));
-console.log(dietas.area);
-    if (dietasrecibidas.area = "Pediatria"){
-
-        console.log("prueba llamar al array directo" + dietasrecibidas);
-        console.log("prueba valor del array traido de sorage  " + dietasrecibidas.dietas.area)
+    let dietasrecibidas = JSON.parse(localStorage.getItem('dietas'));
+    dietasrecibidas.sort((a, b) =>{
+        const areaA = a.area.toLowerCase();
+        const areaB = b.area.toLowerCase();
+        if (areaA < areaB){
+            return -1;
+        }
+        if (areaA > areaB){
+            return 1;
+        }
+        return 0;
+    });
+    let html_content = '';
+    
+    for (let i = 0 ; i < dietasrecibidas.length ; i ++) {
+            html_content += `
+                    <tr class="col-12">
+                        <th scope="row">${dietasrecibidas[i].area}</th>
+                        <td>${dietasrecibidas[i].subarea}</td>
+                        <td>${dietasrecibidas[i].ncama}</td>
+                        <td>${dietasrecibidas[i].pnombre}</td>
+                        <td>${dietasrecibidas[i].papellido}</td>
+                        <td>${dietasrecibidas[i].dietaseleccionada}</td>
+                        <td>${dietasrecibidas[i].descartable}</td>
+                    </tr>
+            `            
     }
+    document.getElementById("Tablasfull").innerHTML = html_content;
 };
+        
+
+//document.getElementById("Tablasfull").innerHTML = dietasrecibidas
+//document.setElementById("Tablasfull").innerHTML = traerDatos
+//
+//
+//let dietasTraidas = JSON.parse(localStorage.getItem('dietas'));
+//
+//let traerDatos = function (){
+//    dietasTraidas.forEach(elementos=>{
+//        document.getElementById("Tablasfull").innerHTML =
+//            `<tr class="col-12">
+//                    <th scope="row">${elementos.area}</th>
+//                    <td>${elementos.subarea}</td>
+//                    <td>${elementos.ncama}</td>
+//                    <td>${elementos.pnombre}</td>
+//                    <td>${elementos.papellido}</td>
+//                    <td>${elementos.dietaseleccionada}</td>
+//                    <td>${elementos.descartable}</td>
+//                    <td>${elementos.descartable}</td>
+//            </tr>`
+//            });
+//        }
+
+////              dietasrecibidas[0].area    para llamar al array traido del local storage. [0] llama al primer valor
+//
+//let traerDatos = function () {
+//    let dietasrecibidas = JSON.parse(localStorage.getItem('dietas'));
+//    let html_content = '';
+//    
+//    for (let i = 0 ; i < dietasrecibidas.length ; i ++) {
+////        if (dietasrecibidas[i].area == 'Pediatria'){
+//            html_content += `
+//                    <tr class="col-12">
+//                        <th scope="row">${dietasrecibidas[i].area}</th>
+//                        <td>${dietasrecibidas[i].subarea}</td>
+//                        <td>${dietasrecibidas[i].ncama}</td>
+//                        <td>${dietasrecibidas[i].pnombre}</td>
+//                        <td>${dietasrecibidas[i].papellido}</td>
+//                        <td>${dietasrecibidas[i].dietaseleccionada}</td>
+//                        <td>${dietasrecibidas[i].descartable}</td>
+//                    </tr>
+//            `            
+// //       }
+//    }
+//    document.getElementById("Tablasfull").innerHTML = html_content;
+//};
+     
---
+++ js/tabladatos.js
@@ -0,0 +1,49 @@
+
+let dietasCargadas = JSON.parse(localStorage.getItem('dietas'));
+
+let RecargarDatos= function() { 
+for (let i = 0 ; i < dietasCargadas.length ; i ++) {
+//console.log(dietasCargadas[i].dietaseleccionada);
+    switch (dietasCargadas[i].dietaseleccionada) {
+        case 'Liviana sin sal/cortado' :
+            console.log("pediatiraSinSal")
+           for (let a = 0; i<dietasCargadas.length; a++) {
+               console.log(a)
+            } 
+            break;
+        case 'General c/p. blando':
+            console.log("General c/p. blando")
+            break;
+        case 'General cortado':
+            console.log("General cortado")
+            break;
+        case 'Renal':
+            console.log("pediatiraSinSal")
+            break;
+        case 'Liviana sin sal c/p. blando':
+            console.log("Liviana sin sal c/p. blando")
+            break;
+        case 'Liviana sin sal/cortado':
+            console.log("Liviana sin sal/cortado")
+            break;
+        case 'Blando':
+            console.log("Blando")
+            break;
+        case 'Blando con pollo':
+            console.log("Blando con pollo")
+            break;
+        case 'Astingente':
+            console.log("Astingente")
+            break;
+        case 'DBT':
+            console.log("DBT")
+            break;
+        case 'Liquado':
+            console.log("Liquado")
+            break;
+        case 'Liquido':
+            console.log("Liquido")
+            break;
+            default:
+                console.log("no hay dietas cargadas")
+        }}}
